f2cd2e1dc98a3329f0e417180cc9b5066db15612
Leonid <leomalinovsky@gmail.com>
2018-10-26 04:21:32 +0300

    package + gulpfile fix

--- gulpfile.js
+++ gulpfile.js
@@ -7,33 +7,39 @@ const sass = require('gulp-sass')
 const minify = require('gulp-uglify')
 const imagemin = require('gulp-imagemin')
 const autoprefixer = require('gulp-autoprefixer')
-//const concat = require('gulp-concat')
+const concat = require('gulp-concat')
+const babel = require('gulp-babel')
 
-gulp.task('clean', function () {
-  return gulp.src('./dist', {
+gulp.task('clean', () =>
+ 	gulp.src('./dist', {
       read: false
     })
     .pipe(clean())
-})
+)
 
-gulp.task('minify', function () {
-	return gulp.src('./src/js/**/*.js')
+gulp.task('minjs', () =>
+ gulp.src('./src/js/**/*.js')
+ 	.pipe(babel({
+  	presets: ['@babel/env']
+  }))
   .pipe(minify())
+  .pipe(concat('script.min.js'))
   .pipe(gulp.dest('./dist/js'))
-})
+)
 
-gulp.task('sass', function () {
-	return gulp.src('./src/scss/*.scss')
-		.pipe(sass())
+gulp.task('sass', () =>
+ gulp.src('./src/scss/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(autoprefixer({
 	    browsers: ['last 2 versions'],
 	    cascade: false
 		}))
+		.pipe(concat('style.min.css'))
 		.pipe(gulp.dest('./dist/css'))
-})
+)
 
-gulp.task('imagemin', function () {
-  return gulp.src('./src/img/*')
+gulp.task('imagemin', () =>
+	gulp.src('./src/img/*')
 	  .pipe(imagemin({
 	    interlaced: true,
 	    progressive: true,
@@ -42,18 +48,18 @@ gulp.task('imagemin', function () {
 	    }]
 	  }))
 	  .pipe(gulp.dest('./dist/img'))
+)
+
+gulp.task('build', ['sass', 'minjs', 'imagemin'], () => {
+	gulp.src('./src/fonts/*').pipe(gulp.dest('./dist/fonts'))
+	
 })
 
-gulp.task('build', ['clean', 'sass', 'minify', 'imagemin'], function () {})
-
-gulp.task('dev', ['clean', 'sass', 'imagemin'], function () {
+gulp.task('dev', ['build'], () => {
 	browserSync.init({
     server: "./"
 	})
-	gulp.src('./src/fonts/*').pipe(gulp.dest('./dist/fonts'))
+	gulp.watch('./src/js/**/*.js', ['minjs']).on('change', browserSync.reload)
 	gulp.watch('./src/scss/*.scss', ['sass']).on('change', browserSync.reload)
 	gulp.watch('./index.html').on('change', browserSync.reload)
 })
-
-// gulp.task('default', function(){
-// });
